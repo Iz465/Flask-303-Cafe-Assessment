3ee873967f7b576babb9272baaca498399952d49
William Möller <williammoller321@gmail.com>
2024-10-06 05:55:45 +1300

    rework of databasemanager

--- datamanager.py
+++ datamanager.py
@@ -4,52 +4,72 @@ import os, sys
 
 connect = sqlite3.connect('database.db') 
 cur = connect.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS MENU(title TEXT, description TEXT, price TEXT, img_url TEXT)") 
+class DatabaseManager:
+    def __init__(self):
+        self.data = []
+        self.insertitems_execute = "INSERT INTO MENU VALUES(?, ?, ?, ?)"
+        self.getitems_excecute = "SELECT title, description, price FROM MENU"
+        self.commonfunc = [self.show_tables.__name__,self.show_values.__name__, self.newitem.__name__]
 
-class Menuitem:
-  def __init__(self):
-    self.title = input("title: ")
-    self.desc = input("description: ")
-    self.price = input("price: ")
-    self.img_url = input("Img url: ")
-
-def printtables():
-    res = cur.execute("SELECT name FROM sqlite_master")
-    return res.fetchall()
-def printvalues():
-    print("Printing:")
-    res = cur.execute("SELECT title, description FROM MENU")
-    itemlist = res.fetchall()
-    for item in itemlist:
-        print(item)
-def join(itemlist):
-    data = []
-    for item in itemlist:
-        data.append([item.title,item.desc,item.price,item.img_url])
-        print(item.title)
+    def show_tables(self):
+        res = cur.execute("SELECT name FROM sqlite_master")
+        tableslist = res.fetchall()
+        print(len(tableslist))
+        for table in tableslist:
+            print(table)
+    def newitem(self):
+        data = []
+        for key in self.data:
+            value = input(f"{key}: ")
+            data.append(value)
+        cur.execute(self.insertitems_execute, data)
+        connect.commit() 
     
-    cur.executemany("INSERT INTO MENU VALUES(?, ?, ?, ?)", data)
-    connect.commit()   
+    def show_values(self):
+        res = cur.execute(self.getitems_excecute)
+        itemlist = res.fetchall()
+        for item in itemlist:
+            print(item)
+    
+
+class Menu(DatabaseManager):
+    def __init__(self):
+        super().__init__()
+        cur.execute("CREATE TABLE IF NOT EXISTS MENU(title TEXT, description TEXT, price TEXT, img_url TEXT)")
+        self.data = ["title", "description", "price", "Img url"]
+        self.insertitems_execute = "INSERT INTO MENU VALUES(?, ?, ?, ?)"
+        self.getitems_excecute = "SELECT title, description, price FROM MENU"
+    
+
+def managedatabases(obj):
+    count = 0
+    for i in obj.commonfunc:
+        count += 1
+        print(f"{count}: {i}")
+    ans = input("input: ")
+    if ans == "1":
+        obj.show_tables()
+    if ans == "2":
+        obj.show_values()
+    if ans == "3":
+        obj.newitem()
+    
+    
+
 
 
 
 def adminmenu():
-    ans = input("Input: ")
-    if ans == "1":
-        itemlist = []
-        item = Menuitem()
-        itemlist.append(item)
-        join(itemlist)
-    if ans == "2":
-        printvalues()
+    x = input("Input: ")
+    if x == "1":
+        menumanager = Menu()
+        managedatabases(menumanager)
+
 
 
 def main():
-    print("select option \n1: all database tables:")
+    print("select option \n1: Menu manager: ")
     adminmenu()
-    fetch = printtables()
-    for i in fetch:
-        print(i)
     connect.close()
 
 if __name__ == '__main__': 
