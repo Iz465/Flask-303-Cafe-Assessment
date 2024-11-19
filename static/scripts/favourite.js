202e4db5ec0640140b168ce04029cb6ea73c9b81
Isak <126728670+Iz465@users.noreply.github.com>
2024-11-19 16:01:51 +1300

    more styling on employment and favouriting part

--- static/scripts/favourite.js
+++ static/scripts/favourite.js
@@ -5,8 +5,13 @@ const open_check = document.getElementById('favourite-popup-id');
 
 if (open_check.style.visibility == 'visible') {
   open_check.style.visibility = 'hidden';
+  open_check.style.marginLeft= '-45%';
 }
 else {
   open_check.style.visibility= 'visible';
+  open_check.style.marginLeft= '0%';
 }
+
+
+
 }
